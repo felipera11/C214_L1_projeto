f3b1d200e0b68a5510598250ddbbf5c1b6684b69
felipera11 <felipe.s@ges.inatel.br>
2024-04-10 20:44:41 -0300

    updated models for better configuration

--- recipes/models.py
+++ recipes/models.py
@@ -2,23 +2,24 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
-
+    
     def __str__(self):
         return self.name
 
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.CharField(max_length=100)
+    unit = models.CharField(max_length=20)  # New field for unit of measurement
+   
+    def __str__(self):
+        return f'{self.quantity} {self.unit} of {self.ingredient.name}'
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
-    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient')
+    ingredients = models.ManyToManyField(RecipeIngredient)
     instructions = models.TextField()
 
     def __str__(self):
         return self.name
 
-class RecipeIngredient(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    quantity = models.CharField(max_length=100)
-    unit = models.CharField(max_length=20)  # New field for unit of measurement
 
-    def __str__(self):
-        return f'{self.quantity} {self.unit} of {self.ingredient.name}'
