6a9c1d6b199287953de1247befb7cd49738057b8
felipera11 <felipe.s@ges.inatel.br>
2024-04-10 17:11:05 -0300

    created models for recipes and ingredients

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+    quantity = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = models.ManyToManyField(Ingredient)
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.name
