e42462c6945274df6b73dc35effed5a2a774fc19
felipera11 <felipe.s@ges.inatel.br>
2024-05-24 16:39:48 -0300

    feat: Add basic edit functionality to recipe page

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:recipe_id>/', views.recipe, name='recipe')
+    path('<int:recipe_id>/', views.recipe, name='recipe'),
+    path('<int:recipe_id>/edit/', views.edit, name='edit_recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Recipe
+from .models import Recipe, RecipeIngredient
 
 def detail(request, recipe_id):
     return HttpResponse("You're looking at recipe %s." % recipe_id)
@@ -12,3 +12,22 @@ def index(request):
 def recipe(request, recipe_id):
     recipe = Recipe.objects.get(pk=recipe_id)
     return render(request, 'recipe.html', {'recipe': recipe})
+
+def edit(request, recipe_id):
+    recipe = Recipe.objects.get(pk=recipe_id)
+    all_recipe_ingredients = RecipeIngredient.objects.all()
+    if request.method == 'POST':
+        recipe.name = request.POST.get('name')
+        recipe.instructions = request.POST.get('instructions')
+        
+        # Get the list of recipeIngredient IDs from the form
+        recipe_ingredient_ids = request.POST.getlist('recipe_ingredients')
+        
+        # Clear existing recipe ingredients and add the new ones
+        recipe.ingredients.clear()
+        for recipe_ingredient_id in recipe_ingredient_ids:
+            recipe.ingredients.add(recipe_ingredient_id)
+        
+        recipe.save()
+        return redirect('recipe', recipe_id=recipe_id)
+    return render(request, 'edit.html', {'recipe': recipe, 'all_recipe_ingredients': all_recipe_ingredients})
